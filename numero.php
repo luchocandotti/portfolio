29660438f9f2d94ee2ea21ba6d635144279f8abb
Luciano Candotti <lcandotti@gmail.com>
2026-03-17 11:39:39 -0300

    agregue contador de visitas arcaico

---
+++ numero.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+// Verificar que esté logueado
+if (!isset($_SESSION['logueado'])) {
+    die('No autorizado');
+}
+
+// Archivo donde se guarda el número
+$archivo = 'numero.txt';
+
+// Si es una petición GET, devolver el número actual
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (file_exists($archivo)) {
+        echo file_get_contents($archivo);
+    } else {
+        echo '000';
+    }
+    exit;
+}
+
+// Si es una petición POST, guardar el nuevo número
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $numero = $_POST['numero'] ?? '000';
+    // Validar que sea solo números
+    $numero = preg_replace('/[^0-9]/', '', $numero);
+    file_put_contents($archivo, $numero);
+    echo $numero;
+    exit;
+}
+?>
